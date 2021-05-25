3c4224bc780369c162ce236ead994e0299601b88
isuruRuhu <isuru.ruhu@gmail.com>
2021-05-25 10:18:48 +0530

    Address the comments

--- src/main/java/org/ballerinax/datamapper/SampleDataAnalysisTask.java
+++ src/main/java/org/ballerinax/datamapper/SampleDataAnalysisTask.java
@@ -121,11 +121,11 @@ public class SampleDataAnalysisTask implements AnalysisTask<CompilationAnalysisC
         for (String moduleName : listOfModuleNames) {
             processSampleDataFiles(moduleName);
         }
-        if (!this.dataMapperLog.getDataMapperPluginDiagnostic().isEmpty()) {
+//        if (!this.dataMapperLog.getDataMapperPluginDiagnostic().isEmpty()) {
             for (Diagnostic diagnostic : this.dataMapperLog.getDataMapperPluginDiagnostic()) {
                 compilationAnalysisContext.reportDiagnostic(diagnostic);
             }
-        }
+//        }
     }
 
     private void processSampleDataFiles(String moduleName) {
